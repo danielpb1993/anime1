94af32c5660ae54887335eba7740aceaaa375624
daniel <danielpb.1993@gmail.com>
2021-11-22 18:42:45 +0100

    modifying AnimeController

--- src/main/java/com/example/anime1/controller/AnimeController.java
+++ src/main/java/com/example/anime1/controller/AnimeController.java
@@ -5,17 +5,23 @@ import com.example.anime1.repository.AnimeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/anime")
+@RequestMapping("/animes")
 public class AnimeController {
 
     @Autowired
     private AnimeRepository animeRepository;
 
     @GetMapping("/")
-    public List<Anime> findAllÇAnime() {
+    public List<Anime> findAllAnime() {
         return animeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Anime getAnimeId(@PathVariable UUID id) {
+        return animeRepository.findById(id).orElse(null);
     }
 
     @PostMapping("/")
