0f2feab70ede718e90e7874e21462219d271949f
daniel <danielpb.1993@gmail.com>
2021-11-23 21:02:21 +0100

    deleteMapping in process

--- src/main/java/com/example/anime1/controller/AnimeController.java
+++ src/main/java/com/example/anime1/controller/AnimeController.java
@@ -1,8 +1,13 @@
 package com.example.anime1.controller;
 
+import com.example.anime1.domain.dto.ErrorMessage;
+import com.example.anime1.domain.dto.ListResult;
 import com.example.anime1.domain.model.Anime;
 import com.example.anime1.repository.AnimeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
@@ -15,17 +20,31 @@ public class AnimeController {
     private AnimeRepository animeRepository;
 
     @GetMapping("/")
-    public List<Anime> findAllAnime() {
-        return animeRepository.findAll();
+    public ResponseEntity<?> findAllAnime() {
+        List<Anime> animeList = animeRepository.findAll();
+        return ResponseEntity.ok().body(ListResult.list(animeList));
     }
 
     @GetMapping("/{id}")
-    public Anime getAnimeId(@PathVariable UUID id) {
-        return animeRepository.findById(id).orElse(null);
+    public ResponseEntity<?> getAnimeId(@PathVariable UUID id) {
+        Anime anime = animeRepository.findById(id).orElse(null);
+        if (anime == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(ErrorMessage.message("No s'ha trobat l'anime amd id \'" + id + "\'"));
+        }
+
+      return ResponseEntity.ok().body(anime);
     }
 
     @PostMapping("/")
-    public Anime createMovie(@RequestBody Anime anime) {
-        return animeRepository.save(anime);
+    public ResponseEntity<?> addAnime(@RequestBody Anime anime) {
+        if (animeRepository.findByname(anime.name) != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body(ErrorMessage.message("Ja existeix un anime amb el nom \'" + anime.name + "\'"));
+        }
+        return ResponseEntity.ok().body(animeRepository.save(anime));
     }
+
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<?> deleteAnime()
 }
---
+++ src/main/java/com/example/anime1/domain/dto/FileResult.java
@@ -0,0 +1,7 @@
+package com.example.anime1.domain.dto;
+import java.util.UUID;
+
+public class FileResult {
+    public UUID fileid;
+    public String contenttype;
+}
